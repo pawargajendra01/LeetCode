40f8f0ef9f611dc2da1f5fa5ef8eedd5c95a76ac
Gajendra Pawar <gajendra101004@gmail.com>
2026-02-22 20:54:57 +0530

    Time: 89 ms (26.21%), Space: 48 MB (28.95%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.java
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.java
@@ -1,29 +1,19 @@
 class Solution {
     public String[] findRestaurant(String[] list1, String[] list2) {
-        int sum=Integer.MAX_VALUE;
-        ArrayList<String> list = new ArrayList<>();
-        for(int i=0;i<list1.length;i++){
-            int temp=Integer.MAX_VALUE;
-            for(int j=0;j<list2.length;j++){
-                if(list1[i].equals(list2[j])){
-                    temp = i + j;
+        int min = 10000;
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i = 0; i < list1.length; i++)
+            for (int j = 0; j < list2.length; j++)
+                if (list1[i].equals(list2[j])) {
+                    if (i + j <= min) {
+                        if (i + j < min) {
+                            arr.clear();
+                            min = i + j;
+                        }
+                        arr.add(list1[i]);
+                    }
                     break;
                 }
-            }
-            if(temp<sum){
-                sum=temp;
-            }
-        }
-        for(int i=0;i<list1.length;i++){
-            for(int j=0;j<list2.length;j++){
-                if(list1[i].equals(list2[j])){
-                    if(i+j==sum){
-                        list.add(list1[i]);
-                        break;
-                    }
-                }
-            }
-        }
-        return list.toArray(new String[0]);
+        return arr.toArray(new String[0]);
     }
 }
