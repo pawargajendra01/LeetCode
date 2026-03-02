3ca339dd8bd31ccf19f172592f5f9a2501b98173
Gajendra Pawar <gajendra101004@gmail.com>
2026-03-02 18:43:37 +0530

    Time: 1 ms (9.79%), Space: 48.2 MB (85.93%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,19 +1,11 @@
 class Solution {
     public int search(int[] nums, int target) {
 
-        boolean found =false;
-
         for(int i=0;i<nums.length;i++){
             if(nums[i]==target){
-                found = true;
                 return i;
             }
         }
-
-        if(!found){
-            return - 1;
-        }
-        else
-            return 0;
+        return -1;
     }
 }
