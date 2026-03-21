2b0f4957d67d75b377dc716424590914d5e8bab3
Gajendra Pawar <gajendra101004@gmail.com>
2026-03-22 00:41:37 +0530

    Time: 13 ms (57.48%), Space: 46.4 MB (82.27%) - LeetHub

---
+++ 0929-unique-email-addresses/0929-unique-email-addresses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        HashSet<String> set = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+        for (String i : emails) {
+            boolean plus = false;
+            for (int j = 0; j < i.length(); j++) {
+                if(i.charAt(j)=='@'){
+                    sb.append(i.substring(j));
+                    break;
+                }
+                else if(i.charAt(j)=='+'){
+                    plus = true;
+                }
+                else if(i.charAt(j)!='.' && !plus){
+                    sb.append(i.charAt(j));
+                }
+                else if(i.charAt(j)=='@'){
+                    sb.append(i.substring(j));
+                    break;
+                }
+            }
+            set.add(sb.toString());
+            sb.setLength(0);
+        }
+        return set.size();
+    }
+}
