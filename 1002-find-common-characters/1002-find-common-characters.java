0b80b700ad73cb8722b387a5190057984fbfa43c
Gajendra Pawar <gajendra101004@gmail.com>
2026-03-31 11:14:14 +0530

    Time: 14 ms (16.96%), Space: 44.1 MB (98.67%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> commonChars(String[] words) {
+        ArrayList<String> list = new ArrayList<>();
+        for(int j=0;j<words[0].length();j++){
+            list.add(Character.toString(words[0].charAt(j)));
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<words.length;i++){
+            sb.append(words[i]);
+            for(int j=0;j<list.size();j++){
+                boolean found = false;
+                for(int k=0;k<sb.length();k++){
+                    if(list.get(j).charAt(0)==sb.charAt(k)){
+                        found=true;
+                        sb.setCharAt(k, '*');
+                        break;
+                    }
+                }
+                if(!found){
+                    list.remove(j);
+                    j--;
+                }
+            }
+            sb.setLength(0);
+        }
+        return list;
+    }
+}
